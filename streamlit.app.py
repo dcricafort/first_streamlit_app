2bd022378b6b6a3d6cd005eedbe34f332a995cb0
dcricafort <124671047+dcricafort@users.noreply.github.com>
2023-02-15 19:32:14 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -27,17 +27,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice
+    #streamlit.write("The user entered ", fruit_choice)
+    streamlit.write("Please select a fruit to get information")
+  else
+    #import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json())
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# get the json values and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output as table
-streamlit.dataframe(fruityvice_normalized)
+    # get the json values and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output as table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 # do not do anything beyond this line
 streamlit.stop()
 
